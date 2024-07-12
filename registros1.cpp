f39162ee7e9241f59e0a7f81aec6079284ab0a03
wsalazarq <wsalazar@unjbg.edu.pe>
2024-07-12 14:46:36 -0500

    regitros_1: Hallar e imprimir los datos de los empleados con el sueldo maximo y minimo

--- registros1.cpp
+++ registros1.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
+struct Empleado{
+    string nombre;
+    char sexo;
+    float sueldo;
+};
+
 int main(){
-    struct Empleado{
-        string nombre;
-        char sexo;
-        float sueldo;
-    };
 
     int n;
     cout<<"Ingrese el numero de trabajadores: ";cin>>n;
@@ -17,10 +18,38 @@ int main(){
         cin.ignore(); 
         cout<<"Ingrese el nombre del empleado "<<i + 1<<" : "; 
         getline(cin, Datos[i].nombre);
-        cout<<"Ingrese el sexo del empleado "<<i + 1<<" Masculino(M)  Femenino(F): ";cin>>Datos[i].sexo;
+         cout<<"Ingrese el sexo del empleado "<<i + 1<<" Masculino(M)  Femenino(F): ";cin>>Datos[i].sexo;
         cout<<"Ingrese el sueldo del empleado "<<i + 1<<" : ";cin>>Datos[i].sueldo;
         cout<<endl;
     }
     
+    float MaxSalario, MinSalario;
+    MaxSalario = Datos[0].sueldo;
+    MinSalario = Datos[0].sueldo;
+    int min = 0, max = 0;
+
+    for (int i = 0; i < n; i++){
+        
+        if (Datos[i].sueldo > MaxSalario){
+            MaxSalario = Datos[i].sueldo;
+            max = i;
+        }
+
+        if (Datos[i].sueldo < MinSalario){
+            MinSalario = Datos[i].sueldo;
+            min = i;
+        }
+    }
+
+    cout<<"Datos del empleado con mayor sueldo: "<<endl;
+    cout<<"Nombre :"<<Datos[max].nombre<<endl;
+    cout<<"Sexo :"<<Datos[max].sexo<<endl;
+    cout<<"Sueldo :"<<Datos[max].sueldo<<endl;
+
+    cout<<"Datos del empleado con menor sueldo: "<<endl;
+    cout<<"Nombre :"<<Datos[min].nombre<<endl;
+    cout<<"Sexo :"<<Datos[min].sexo<<endl;
+    cout<<"Sueldo :"<<Datos[min].sueldo<<endl;
+
     return 0;
 }
