5755cd786098ea532b006e6525700651cb79b3d1
wsalazarq <wsalazar@unjbg.edu.pe>
2024-07-12 14:02:33 -0500

    regitros_1: Implementar el registro Empleado y los datos correspondientes

---
+++ registros1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    struct Empleado{
+        string nombres;
+        string sexo;
+        float sueldo;
+    };
+
+    int n;
+    int Datos[n];
+    
+    cout<<"Ingrese el numero de trabajadores: ";cin>>n;
+    for (int i = 0; i < n; i++){
+        
+    }
+    
+    return 0;
+}
