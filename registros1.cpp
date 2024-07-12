d335fc52a2f2c364092187cd7bb5b64f5911a987
wsalazarq <wsalazar@unjbg.edu.pe>
2024-07-12 14:18:41 -0500

    regitros_1: Implementar la lectura de los datos en el arreglo

--- registros1.cpp
+++ registros1.cpp
@@ -3,17 +3,23 @@ using namespace std;
 
 int main(){
     struct Empleado{
-        string nombres;
-        string sexo;
+        string nombre;
+        char sexo;
         float sueldo;
     };
 
     int n;
-    int Datos[n];
-    
     cout<<"Ingrese el numero de trabajadores: ";cin>>n;
+    
+    Empleado Datos[n];
+
     for (int i = 0; i < n; i++){
-        
+        cin.ignore(); 
+        cout<<"Ingrese el nombre del empleado "<<i + 1<<" : "; 
+        getline(cin, Datos[i].nombre);
+        cout<<"Ingrese el sexo del empleado "<<i + 1<<" Masculino(M)  Femenino(F): ";cin>>Datos[i].sexo;
+        cout<<"Ingrese el sueldo del empleado "<<i + 1<<" : ";cin>>Datos[i].sueldo;
+        cout<<endl;
     }
     
     return 0;
