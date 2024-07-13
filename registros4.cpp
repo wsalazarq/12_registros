5e67ada707b0fb6d1c5378d162caef07fd22b424
wsalazarq <wsalazar@unjbg.edu.pe>
2024-07-13 10:23:47 -0500

    registros_4: Implementar la busqueda del atleta con mas medallas del pais ingresado

--- registros4.cpp
+++ registros4.cpp
@@ -26,6 +26,8 @@ int main(){
     string paisIngresado;
     cout<<"Ingrese un pais: ";cin>>paisIngresado;
 
+    int maxMedallas = 0;
+    int atletaMedallas; 
     bool encontrado = false;
 
     for (int i = 0; i < n; i++){
@@ -36,7 +38,23 @@ int main(){
             cout<<"Disciplina: "<<atletas[i].disciplina<<endl;
             cout<<"Numero de medallas: "<<atletas[i].numMedallas<<endl;
             cout<<endl;
+
+            if (atletas[i].numMedallas > maxMedallas){
+                maxMedallas = atletas[i].numMedallas;
+                atletaMedallas = i;
+            }
         }
     }
+
+    if (encontrado == false){
+        cout<<"No se encontro un atleta de ese pais"<<endl;
+    }else{
+        cout<<"Atleta con mas madallas del pais "<<paisIngresado<<" : "<<endl;
+        cout<<"Nombre: "<<atletas[atletaMedallas].nombres<<endl;
+        cout<<"Pais: "<<atletas[atletaMedallas].pais<<endl;
+        cout<<"Disciplina: "<<atletas[atletaMedallas].disciplina<<endl;
+        cout<<"Numero de medallas: "<<atletas[atletaMedallas].numMedallas<<endl;
+    }
+    
     return 0;
 }
